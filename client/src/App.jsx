b994e878a23aac64d73097059b76fb18d6a8d2a5
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-05-24 19:22:01 +0200

    add/frontend and back connection with axios on app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,41 @@
 import { useState, useEffect } from 'react'
+import axios from "axios";
 import './App.css'
 
 function App() {
 
 const [products, setProducts] = useState([]);
+const [error, setError] = useState(null);
 
 const API_URL = "http://localhost:3001/api/products";
 
-// Fetch all products
 useEffect(() => {
-  fetch(API_URL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error fetching products");
-      }
-      return response.json();
-    })
-    .then((data) => setProducts(data))
-    .catch((error) => setError(error.message));
-}, []);
+  async function fetchData() {
+    try{
+      // Petición HTTP
+      const res = await axios.get(API_URL);
+      const json = res.data;
+
+      // Guarda en el array de posts el resultado. Procesa los datos
+      setProducts(json);   
+    } catch(e){
+setProducts ([])     }
+  }
+  fetchData();
+}, []); 
+
+// Fetch all products
+// useEffect(() => {
+//   fetch(API_URL)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Error fetching products");
+//       }
+//       return response.json();
+//     })
+//     .then((data) => setProducts(data))
+//     .catch((error) => setError(error.message));
+// }, []);
 
   return (
     <>
