1689dfa0b532182d8e49f004a05e8768142febb4
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-05-27 19:40:09 +0200

    added providers in search bar. users can now search for providers and products at the same time

--- queries/products.queries.js
+++ queries/products.queries.js
@@ -2,6 +2,19 @@ const getAllProducts = `SELECT
       *
     FROM products;`;
 
+const getProductsWithProviderName = `
+    SELECT 
+      products.id_product,
+      products.name,
+      products.price,
+      products.img_url,
+      products.relevancy,
+      products.provider_id,
+      providers.name AS provider_name
+    FROM products
+    JOIN providers ON products.provider_id = providers.id_provider
+  `;  
+
 const createProduct = `
     INSERT INTO products (img_url, name, price, relevancy, provider_id)
     VALUES ($1, $2, $3, $4, $5)
@@ -12,4 +25,4 @@ const deleteProduct = `DELETE FROM products
     RETURNING *`;
 
 
-module.exports = {getAllProducts, createProduct, deleteProduct}
+module.exports = {getAllProducts, getProductsWithProviderName, createProduct, deleteProduct}
